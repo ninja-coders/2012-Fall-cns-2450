dd6888e8a2adbd15bbc89d8265596bff2f649f33
Michael Wright <mkwright@gmail.com>
2012-10-09 15:19:22 -0600

    Added the base .net project to the repository (TDDSample project)

---
+++ testing/csharp/TDDSample/TDDSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TDDSample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Michael Wright");
+      Console.WriteLine("CS 2450\n");
+
+      Console.Write("Enter a number to get a factorial: ");
+      int factorialNumber = 0;
+      int.TryParse(Console.ReadLine(), out factorialNumber);
+
+      int sum = 0;
+      for (int i = 0; i < factorialNumber; ++i)
+      {
+        sum *= i;
+      }
+      Console.WriteLine("Factorial Value is {0}", sum);
+      Console.ReadLine();
+    }
+  }
+}
